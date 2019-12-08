4b7db0b07cf3cb73b32142a4440bc6b014c3c3f2
clinsm <30928377+clinsm@users.noreply.github.com>
2019-12-08 19:02:49 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <title>Car News</title>
   </head>
   <body>
@@ -28,13 +29,71 @@
       </div>
     </div>
     <div class="section1">
-      <img src="img/drive.jpg" alt="image">
-      <div class="welcome">
+      <div class="welcome" align='left'>
         <div class="bigmessage">
             THE HOME OF CAR ENTHUSISASTS
         </div>
         <div class="message">
           A good guide for anyone looking for a good read
+        </div>
+      </div>
+      <img src="img/drive.jpg" alt="image">
+    </div>
+    <div class="section2">
+      <div class="bigrow1">
+        <div class="info">
+          Latest news
+        </div>
+        <div class="row1">
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/toyota.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <div class="tail">
+              <a href="#">Read</a>
+            </div>
+          </div>
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/suv.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head2
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <div class="tail">
+              <a href="#">Read</a>
+            </div>
+          </div>
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/interior.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head3
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <div class="tail">
+              <a href="#">Read</a>
+            </div>
+          </div>
+
+          <div class="col2">
+            ad space
+          </div>
         </div>
       </div>
     </div>
@@ -43,6 +102,19 @@
     function myFunction(x) {
       x.classList.toggle("change");
     }
+
+    var myNav = document.getElementById('mynav');
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    }
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
     </script>
   </body>
 </html>
