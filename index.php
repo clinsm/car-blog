177ad97aaddccdbcce544e75539bc826c0bdf010
clinsm <30928377+clinsm@users.noreply.github.com>
2020-01-17 17:45:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <title>Car News</title>
+  </head>
+  <body>
+    <div class="navbar" id="nav" align='center'>
+      <div class="menu" onclick="myFunction(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </div>
+      <div class="ph">
+      </div>
+      <div class="logo">
+        <img src="img/logo.svg" alt="" width="40" height="40">
+      </div>
+      <div class="plh">
+      </div>
+      <div class="searchbox">
+        <input type="text" class="search" name="search" value="" placeholder="search">
+        <img src="img/search.svg" alt="find" width="20" height="20">
+      </div>
+      <div class="account">
+        <img src="img/account.svg" height="30" width="30" alt="account">
+      </div>
+    </div>
+    <div class="section1">
+      <div class="welcome" align='left'>
+        <div class="bigmessage">
+            THE HOME OF CAR ENTHUSISASTS
+        </div>
+        <div class="message">
+          A good guide for anyone looking for a good read
+        </div>
+      </div>
+      <img src="img/drive.jpg" alt="image">
+    </div>
+    <div class="section2">
+      <div class="bigrow1">
+        <div class="info">
+          Latest news
+        </div>
+        <div class="row1">
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/toyota.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <a href="news.html">
+              <div class="tail">
+                Read
+              </div>
+            </a>
+
+          </div>
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/suv.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head2
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <a href="#">
+              <div class="tail">
+                Read
+              </div>
+            </a>
+          </div>
+
+          <div class="ad1">
+            ad space
+          </div>
+        </div>
+        <!--second sub row-->
+        <div class="row1">
+
+          <div class="ad2">
+            ad space
+          </div>
+
+          <div class="col1">
+            <div class="head">
+              <img src="img/interior.jpg" width="50" height="50" alt="head">
+            </div>
+            <div class="midup">
+              Recent news head3
+            </div>
+            <div class="midlow">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+            </div>
+            <a href="#">
+              <div class="tail">
+                Read
+              </div>
+            </a>
+          </div>
+
+          </div>
+          <a href="#" class="load">
+            <div class="more">
+              <p>Load more</p>
+              <img src="img/plus.svg" alt="more" width="30" height="30">
+            </div>
+          </a>
+      </div>
+    </div>
+    <!--3rd section for reviews-->
+    <div class="section3">
+      <div class="info">
+      Car reviews
+      </div>
+      <div class="row2">
+        <div class="col2">
+          <div class="top2a" align = "center">
+            <img src="img/bmw.png" alt="car">
+          </div>
+          <div class="top2b">
+            BMW X4
+          </div>
+          <div class="mid2a">
+            <img src="img/speed.svg" alt="speed" height="20" width="20">
+            <p class="txt" > 220Kph</p>
+          </div>
+          <div class="mid2b">
+            <img src="img/coins.svg" alt="speed" height="20" width="20">
+            <p class="txt" >From US$3,772.00</p>
+          </div>
+          <div class="btm2a">
+            Review summary   Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet
+          </div>
+          <div class="btm2b">
+            <div class="rtng" id="rtng1" style="  background: #fcc90f;">
+            </div>
+            <div class="rtng" id="rtng2">
+            </div>
+            <div class="rtng" id="rtng3">
+            </div>
+            <div class="rtng" id="rtng4">
+            </div>
+            <div class="rtng" id="rtng5">
+            </div>
+          </div>
+        </div>
+        <!--Anothr column-->
+        <div class="col2">
+          <div class="top2a" align = "center">
+            <img src="img/mini.png" alt="car">
+          </div>
+          <div class="top2b">
+            Mini Cooper Clubman
+          </div>
+          <div class="mid2a">
+            <img src="img/speed.svg" alt="speed" height="20" width="20">
+            <p class="txt" >Car Speed</p>
+          </div>
+          <div class="mid2b">
+            <img src="img/coins.svg" alt="speed" height="20" width="20">
+            <p class="txt" >From US$2,542.00</p>
+          </div>
+          <div class="btm2a">
+            Review summary
+          </div>
+          <div class="btm2b">
+            <div class="rtng" id="rtng1">
+            </div>
+            <div class="rtng" id="rtng2">
+            </div>
+            <div class="rtng" id="rtng3">
+            </div>
+            <div class="rtng" id="rtng4">
+            </div>
+            <div class="rtng" id="rtng5">
+            </div>
+          </div>
+        </div>
+        <!--Anothr column-->
+        <div class="col2">
+          <div class="top2a" align = "center">
+            <img src="img/audi.png" alt="car">
+          </div>
+          <div class="top2b">
+            Audi A6
+          </div>
+          <div class="mid2a">
+            <img src="img/speed.svg" alt="speed" height="20" width="20">
+            <p class="txt" >Car Speed</p>
+          </div>
+          <div class="mid2b">
+            <img src="img/coins.svg" alt="speed" height="20" width="20">
+            <p class="txt" >From US$3,772.00</p>
+          </div>
+          <div class="btm2a">
+            Review summary
+          </div>
+          <div class="btm2b">
+            <div class="rtng" id="rtng1">
+            </div>
+            <div class="rtng" id="rtng2">
+            </div>
+            <div class="rtng" id="rtng3">
+            </div>
+            <div class="rtng" id="rtng4">
+            </div>
+            <div class="rtng" id="rtng5">
+            </div>
+          </div>
+        </div>
+        <!--Anothr column but for ads-->
+        <div class="ad3">
+          Ad Space
+        </div>
+
+      </div>
+      <!--Anothr row but for ads-->
+      <div class="ad4">
+        Ad Space
+      </div> <br> <br>
+      <a href="#" class="load">
+        <div class="more">
+          <p>Load more</p>
+          <img src="img/plus.svg" alt="more" width="30" height="30">
+        </div>
+      </a>
+    </div>
+    <div class="footer" align = "center">
+      <!--First footer column-->
+      <div class="f1">
+        <a href="#">
+          <div class="ftop" align = 'center'>
+            <img src="img/rev.svg" alt=""  height="20" width="20">
+            <p>Reviews</p>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="fbtm">
+            <img src="img/news.svg" alt=""  height="20" width="20">
+            <p>News</p>
+          </div>
+        </a>
+      </div>
+      <!--Second footer column-->
+      <div class="f2">
+        <a href="#">
+          <div class="ftop">
+            <img src="img/cars.svg" alt=""  height="20" width="20">
+            <p>Cars</p>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="fbtm">
+            <img src="img/about.svg" alt=""  height="20" width="20">
+            <p>About</p>
+          </div>
+        </a>
+      </div>
+      <!--Third footer column-->
+      <div class="f3">
+        <a href="#">
+          <div class="ftop">
+            <img src="img/user.svg" alt=""  height="20" width="20">
+            <p>Login</p>
+          </div>
+        </a>
+
+        <a href="#">
+          <div class="fbtm">
+            <img src="img/account.svg" alt=""  height="20" width="20">
+            <p>Register</p>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <script>
+    //for changing the nav bar background after scroll
+    var navbar = document.getElementById('nav');
+  window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+      navbar.style.backgroundColor = "#131b1c";
+      navbar.style.boxShadow = "0px 4px 8px -2px rgba(0,0,0,0.75)";
+    } else {
+      navbar.style.backgroundColor = "initial";
+      navbar.style.boxShadow = "inset 1px 32px 35px -26px rgba(0,0,0,0.75)";
+    }
+  }
+  //menu icon
+      function myFunction(x) {
+        x.classList.toggle("change");
+      }
+
+    </script>
+  </body>
+</html>
